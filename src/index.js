c6098b26a63fabe2b2e091c9d74d1b4187d134ed
Florian <florian.martin63000@gmail.com>
2021-06-24 19:06:31 +0200

    Feat: service worker is up. PWA is installable on Desktop

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient, { InMemoryCache, gql } from 'apollo-boost';
 import { BrowserRouter as Router } from 'react-router-dom';
+import * as serviceWorker from './serviceWorker';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000/',
@@ -31,7 +31,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+serviceWorker.register();
